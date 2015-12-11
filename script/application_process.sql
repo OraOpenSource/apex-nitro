643ddc750208c8d7f588c7ecdb86f8c498af72e2
Vincent Morneau <vincent.morneau@gmail.com>
2015-12-11 16:09:13 -0500

    Updated APEX objects

--- script/application_process.sql
+++ script/application_process.sql
@@ -1,14 +1,29 @@
 -- Notes:
 -- 1) Application process name doesn't matter
 -- 2) Load before header
+declare
+    l_exists number;
 begin
-    -- TODO vmorneau: Review logic and comment
-    if OWA_UTIL.GET_CGI_ENV('HTTP_REFERER') like '%localhost%' then
-        apex_application.g_flow_images := SUBSTR(OWA_UTIL.GET_CGI_ENV('HTTP_REFERER'), 1, INSTR(OWA_UTIL.GET_CGI_ENV('HTTP_REFERER'), '/', 1, 3));
-    else
-        execute immediate 'select host from browsersync_host where ''' || OWA_UTIL.GET_CGI_ENV('HTTP_REFERER') || ''' like ''%'' || host || ''%'''
-        into apex_application.g_flow_images;
+    -- inserts the browsersync host
+    if :G_BROWSERSYNC_HOST is not null then
+        begin
+            execute immediate 'insert into browsersync_host values (:1)'
+            using :G_BROWSERSYNC_HOST;
+        exception
+            when dup_val_on_index then
+                null;
+        end;
     end if;
-exception when others then
-    null; -- table doesn't exist or host doesn't exists in table
+
+    -- verifies if the current host exists in browsersync
+    execute immediate 'select distinct 1 from browsersync_host where :1 like ''%'' || host || ''%'''
+    into l_exists
+    using OWA_UTIL.GET_CGI_ENV('HTTP_REFERER');
+
+    -- sets the current host to replace #APP_IMAGES#
+    :G_APP_IMAGES := SUBSTR(OWA_UTIL.GET_CGI_ENV('HTTP_REFERER'), 1, INSTR(OWA_UTIL.GET_CGI_ENV('HTTP_REFERER'), '/', 1, 3));
+exception
+    when no_data_found then
+        -- no table or no data found
+        :G_APP_IMAGES := '#APP_IMAGES#';
 end;
