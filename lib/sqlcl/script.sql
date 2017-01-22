bfd0f2fc29e8e503e3e68a9c57ab92c0877ee7a8
Vincent Morneau <vincent.morneau@gmail.com>
2017-01-22 16:08:05 -0500

    makes the publish feature more robust
    fixes #178

---
+++ lib/sqlcl/script.sql
@@ -0,0 +1,8 @@
+set define on
+
+define DIST_UPLOAD = '&1'
+define DIST_FOLDER = '&2'
+define APP_URL = '&3'
+define APEX_VERSION = '&4'
+
+script "&DIST_UPLOAD." "&DIST_FOLDER." &APP_URL. &APEX_VERSION.
