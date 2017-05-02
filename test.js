c8185de63325cc7eabc6eaf20df1ba3f25ce48bb
Vincent Morneau <vincent.morneau@gmail.com>
2017-05-02 16:24:24 -0400

    makes unit testing more robust

--- test.js
+++ test.js
@@ -78,20 +78,209 @@ test('header-invalid', t => {
 	}
 });
 
-const testDemo = function (name) {
-	test.serial.cb(name, t => {
-		t.plan(1);
+test.serial.cb('demo-simple', t => {
+	t.plan(1);
 
-		launch([name], () => {
-			t.pass();
-			t.end();
-		});
+	const config = {
+		appURL: 'https://apex.oracle.com/pls/apex/f?p=105990:101',
+		srcFolder: path.resolve('./examples/demo-simple/src'),
+		distFolder: path.resolve('./examples/demo-simple/dist'),
+		js: {
+			processor: 'default',
+			concat: false,
+			library: false
+		},
+		css: {
+			language: 'css',
+			concat: false
+		},
+		browsersync: {
+			notify: false,
+			ghostMode: false
+		},
+		header: {
+			enabled: false
+		},
+		apex: {
+			openBuilder: false
+		}
+	};
+
+	launch(['demo-simple'], undefined, config, () => {
+		t.pass();
+		t.end();
 	});
-};
+});
 
-testDemo('demo-simple');
-testDemo('demo-concat');
-testDemo('demo-header');
-testDemo('demo-less');
-testDemo('demo-sass');
-testDemo('demo-webpack');
+test.serial.cb('demo-concat', t => {
+	t.plan(1);
+
+	const config = {
+		appURL: 'https://apex.oracle.com/pls/apex/f?p=105990:101',
+		srcFolder: path.resolve('./examples/demo-concat/src'),
+		distFolder: path.resolve('./examples/demo-concat/dist'),
+		js: {
+			processor: 'default',
+			concat: true,
+			concatFilename: 'app'
+		},
+		css: {
+			language: 'css',
+			concat: true,
+			concatFilename: 'app'
+		},
+		browsersync: {
+			notify: false,
+			ghostMode: false
+		},
+		header: {
+			enabled: false
+		},
+		apex: {
+			openBuilder: false
+		}
+	};
+
+	launch(['demo-concat'], undefined, config, () => {
+		t.pass();
+		t.end();
+	});
+});
+
+test.serial.cb('demo-header', t => {
+	t.plan(1);
+
+	const config = {
+		appURL: 'https://apex.oracle.com/pls/apex/f?p=105990:101',
+		srcFolder: path.resolve('./examples/demo-header/src'),
+		distFolder: path.resolve('./examples/demo-header/dist'),
+		js: {
+			processor: 'default',
+			concat: false
+		},
+		css: {
+			language: 'css',
+			concat: false
+		},
+		browsersync: {
+			notify: false,
+			ghostMode: false
+		},
+		header: {
+			enabled: true,
+			packageJsonPath: path.resolve('./examples/demo-header/package.json')
+		},
+		apex: {
+			openBuilder: false
+		}
+	};
+
+	launch(['demo-header'], undefined, config, () => {
+		t.pass();
+		t.end();
+	});
+});
+
+test.serial.cb('demo-less', t => {
+	t.plan(1);
+
+	const config = {
+		appURL: 'https://apex.oracle.com/pls/apex/f?p=105990:101',
+		srcFolder: path.resolve('./examples/demo-less/src'),
+		distFolder: path.resolve('./examples/demo-less/dist'),
+		js: {
+			processor: 'default',
+			concat: false
+		},
+		css: {
+			language: 'less',
+			concat: false
+		},
+		browsersync: {
+			notify: false,
+			ghostMode: false
+		},
+		header: {
+			enabled: false
+		},
+		apex: {
+			openBuilder: false,
+			apexDestination: 'application'
+		}
+	};
+
+	launch(['demo-less'], undefined, config, () => {
+		t.pass();
+		t.end();
+	});
+});
+
+test.serial.cb('demo-sass', t => {
+	t.plan(1);
+
+	const config = {
+		appURL: 'https://apex.oracle.com/pls/apex/f?p=105990:101',
+		srcFolder: path.resolve('./examples/demo-sass/src'),
+		distFolder: path.resolve('./examples/demo-sass/dist'),
+		js: {
+			processor: 'default',
+			concat: false
+		},
+		css: {
+			language: 'sass',
+			concat: false
+		},
+		browsersync: {
+			notify: false,
+			ghostMode: false
+		},
+		header: {
+			enabled: false
+		},
+		apex: {
+			openBuilder: false
+		}
+	};
+
+	launch(['demo-sass'], undefined, config, () => {
+		t.pass();
+		t.end();
+	});
+});
+
+test.serial.cb('demo-webpack', t => {
+	t.plan(1);
+
+	const config = {
+		appURL: 'https://apex.oracle.com/pls/apex/f?p=105990:1',
+		srcFolder: path.resolve('./examples/demo-webpack/src'),
+		distFolder: path.resolve('./examples/demo-webpack/dist'),
+		js: {
+			processor: 'webpack',
+			concat: false,
+			entries: [path.resolve('./examples/demo-webpack/src/js/app.js')],
+			bundleFilename: 'bundle',
+			library: true,
+			libraryName: 'demo'
+		},
+		css: {
+			language: 'css',
+			concat: false
+		},
+		browsersync: {
+			notify: false,
+			ghostMode: false
+		},
+		header: {
+			enabled: false
+		},
+		apex: {
+			openBuilder: false
+		}
+	};
+
+	launch(['demo-webpack'], undefined, config, () => {
+		t.pass();
+		t.end();
+	});
+});
